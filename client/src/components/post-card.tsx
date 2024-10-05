d1824474ccb22fff20c36c17b2028746fc1d503a
MostafaMGomaa <gomaamostafa26@gmail.com>
2024-10-05 21:18:25 +0300

    feature: postCard

--- client/src/components/post-card.tsx
+++ client/src/components/post-card.tsx
@@ -1,5 +1,62 @@
 import { Post } from '@codersquare/shared';
+import { faHeart as faHeartRegualr } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSoild } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 
-export const PostCard = (post: Post) => {
-  return <div className=""></div>;
+export const PostCard = ({ post }: { post: Post }) => {
+  const [isHover, setIsHover] = useState(false);
+  const postHref = `post/${post.id}`;
+
+  const commentCount = `5 comments`;
+  const likesCount = `43 likes`;
+  const username = 'mostafagomaa';
+  const postedAt = '1 hour ago';
+
+  return (
+    <>
+      <div className="flex items-center gap-x-2 mb-3" key={post.id}>
+        <a href={postHref} className="flex items-center gap-x-2">
+          <FontAwesomeIcon
+            icon={isHover ? faHeartSoild : faHeartRegualr}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+            className="text-gray-400 hover:text-orange-700 transition-colors duration-450"
+          />
+          <p className="font-bold text-gray-600 hover:text-orange-700 ">
+            {capitalizeFirstLetter(post.title)}
+          </p>
+        </a>
+        <span className="text-gray-400"> ({shortenUrl(post.url)})</span>
+        <button
+          className="px-6 py-[1px] ml-5 border border-gray-400 text-gray-400 rounded-md text-center text-sm
+          hover:bg-orange-700 hover:border-transparent hover:text-white"
+        >
+          {commentCount}
+        </button>
+      </div>
+
+      <div className="flex items-center mx-6 gap-x-2 gap-y-4 text-xs text-gray-400">
+        <span>{likesCount}</span>
+        <span>|</span>
+        <a href="" className="font-semibold hover:text-orange-700">
+          {username}
+        </a>
+        <span>|</span>
+        <span>{postedAt}</span>
+      </div>
+
+      {/* Adjusted gap between different posts */}
+      <hr className="my-4 border-gray-200" />
+    </>
+  );
+};
+
+const shortenUrl = (url: string): string => {
+  const withProtocol = url.startsWith('http') ? url : `https://${url}`;
+  return new URL(withProtocol).host;
+};
+
+const capitalizeFirstLetter = (s: string): string => {
+  return s.charAt(0).toUpperCase() + s.slice(1);
 };
--- client/src/pages/list-posts.tsx
+++ client/src/pages/list-posts.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import { Post } from '@codersquare/shared';
 import { getAllPosts } from '../api';
+import { PostCard } from '../components';
 
 export const ListPosts = () => {
   const { data, error, isLoading } = useQuery<Post[]>({
@@ -17,20 +18,9 @@ export const ListPosts = () => {
     return <div>Error loading posts</div>;
   }
 
-  let count = `5 comments`;
-
   return (
-    <div className="flex flex-col gap-y-7 gap-x-0.5 place-items-start justify-center container px-[1rem] py-4">
-      {data &&
-        data.map((post) => (
-          <div className="flex items-center gap-1.5" key={post.id}>
-            <p className="font-bold text-gray-600">{post.title}</p>
-            <span className="text-gray-400"> ({post.url})</span>
-            <button className="border border-gray-400 text-gray-400 px-6 py-[1px] rounded-md text-center text-sm ml-5">
-              {count}
-            </button>
-          </div>
-        ))}
+    <div className="flex flex-col  gap-x-0.5 place-items-start justify-center container px-[1rem] py-4">
+      {data && data.map((post: Post) => <PostCard post={post} />)}
     </div>
   );
 };
