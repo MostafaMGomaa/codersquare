240b800ed0ea4bad362e8fdf55071c012cf89caa
MostafaMGomaa <gomaamostafa26@gmail.com>
2024-10-07 18:48:57 +0300

    refactor signup page

--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -6,44 +6,21 @@ import { SignupPayload } from '../types';
 import { useNavigate } from 'react-router-dom';
 
 export const SignupForm = () => {
-  const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [password, setPassword] = useState('');
-  const [focusedInput, setFocusedInput] = useState<string | null>(null);
-  const [hoverInput, setHoverInput] = useState<{ [key: string]: boolean }>({
-    email: false,
-    firstName: false,
-    lastName: false,
-    password: false,
+  const [signupData, setSignupData] = useState<SignupPayload>({
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: '',
   });
+
   const navigate = useNavigate();
   const signupMutation = useSignupMutation();
-
-  const handleFocus = (inputName: string) => {
-    setFocusedInput(inputName);
-  };
-
-  const handleHover = (inputName: string, isHover: boolean) => {
-    setHoverInput((prev) => ({ ...prev, [inputName]: isHover }));
-  };
-
-  const handleBlur = () => {
-    setFocusedInput(null);
-  };
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
-    const payloadData: SignupPayload = {
-      email,
-      firstName,
-      lastName,
-      password,
-    };
-
     try {
-      await signupMutation.mutateAsync(payloadData);
+      await signupMutation.mutateAsync(signupData);
       toast.success('Successfully Signup!');
       setTimeout(() => {
         navigate('/');
@@ -56,95 +33,78 @@ export const SignupForm = () => {
     }
   };
 
-  const getInputClasses = (inputName: string) => {
-    const isFocused = focusedInput === inputName;
-    const isHovered = hoverInput[inputName] ? true : false;
-    return `border ${
-      isFocused || isHovered ? 'border-orange-700' : 'border-gray-500'
-    } 
-          rounded bg-transparent h-9 w-[20rem] transition-colors duration-300 
-          p-2 outline-none shadow-none focus:ring-0 focus:ring-transparent`;
+  const handleSignupDate = (inputName: string, inputValue: string) => {
+    setSignupData((values: SignupPayload) => ({
+      ...values,
+      [inputName]: inputValue,
+    }));
   };
 
-  const getLabelClasses = (inputName: string) => {
-    const isFocused = focusedInput === inputName;
-    const isHovered = hoverInput[inputName] ? true : false;
-    return `font-semibold w-40 ${
-      isFocused || isHovered ? 'text-orange-700' : 'text-gray-500'
-    }`;
+  const getInputClasses = () => {
+    return `border border-gray-500 
+          group-hover:border-orange-700 rounded bg-transparent h-9 w-[20rem] transition-colors duration-300 
+          p-2 outline-none shadow-none focus:ring-0 focus:ring-transparent group-focus-within:border-orange-700`;
+  };
+
+  const getLabelClasses = () => {
+    return `font-semibold w-40 group-hover:text-orange-700 text-gray-500 group-focus-within:text-orange-700`;
   };
 
   return (
     <div className="m-10 mt-24">
       <form className="flex flex-col gap-10" onSubmit={handleSubmit}>
-        <div className="email flex">
-          <label className={getLabelClasses('email')} htmlFor="email">
+        <div className="email flex group">
+          <label className={getLabelClasses()} htmlFor="email">
             Email
           </label>
           <input
             type="email"
             id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className={getInputClasses('email')}
-            onFocus={() => handleFocus('email')}
-            onMouseEnter={() => handleHover('email', true)}
-            onMouseLeave={() => handleHover('email', false)}
-            onBlur={handleBlur}
+            value={signupData.email}
+            onChange={(e) => handleSignupDate('email', e.target.value)}
+            className={getInputClasses()}
             required
           />
         </div>
 
-        <div className="firstName flex">
-          <label className={getLabelClasses('firstName')} htmlFor="firstName">
+        <div className="firstName flex group">
+          <label className={getLabelClasses()} htmlFor="firstName">
             First Name
           </label>
           <input
             type="text"
             id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            className={getInputClasses('firstName')}
-            onFocus={() => handleFocus('firstName')}
-            onMouseEnter={() => handleHover('firstName', true)}
-            onMouseLeave={() => handleHover('firstName', false)}
-            onBlur={handleBlur}
+            value={signupData.firstName}
+            onChange={(e) => handleSignupDate('firstName', e.target.value)}
+            className={getInputClasses()}
             required
           />
         </div>
 
-        <div className="lastName flex">
-          <label className={getLabelClasses('lastName')} htmlFor="lastName">
+        <div className="lastName flex group">
+          <label className={getLabelClasses()} htmlFor="lastName">
             Last Name
           </label>
           <input
             type="text"
             id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            className={getInputClasses('lastName')}
-            onFocus={() => handleFocus('lastName')}
-            onMouseEnter={() => handleHover('lastName', true)}
-            onMouseLeave={() => handleHover('lastName', false)}
-            onBlur={handleBlur}
+            value={signupData.lastName}
+            onChange={(e) => handleSignupDate('lastName', e.target.value)}
+            className={getInputClasses()}
             required
           />
         </div>
 
-        <div className="password flex">
-          <label className={getLabelClasses('password')} htmlFor="password">
+        <div className="password flex group">
+          <label className={getLabelClasses()} htmlFor="password">
             Password
           </label>
           <input
             type="password"
             id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={getInputClasses('password')}
-            onFocus={() => handleFocus('password')}
-            onMouseEnter={() => handleHover('password', true)}
-            onMouseLeave={() => handleHover('password', false)}
-            onBlur={handleBlur}
+            value={signupData.password}
+            onChange={(e) => handleSignupDate('password', e.target.value)}
+            className={getInputClasses()}
             required
           />
         </div>
