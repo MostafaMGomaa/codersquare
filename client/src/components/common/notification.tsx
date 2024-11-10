a179343e3b2717813e40e17f8810dc30e23c2447
MostafaMGomaa <gomaamostafa26@gmail.com>
2024-11-10 08:11:05 +0200

    feat: fetch notifications

--- client/src/components/common/notification.tsx
+++ client/src/components/common/notification.tsx
@@ -1,14 +1,39 @@
 import { useState } from 'react';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBell,
+  faTriangleExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
-import { Notification as INotification } from '@codersquare/shared';
+import { useQuery } from '@tanstack/react-query';
+import { DataResult, Notification as INotification } from '@codersquare/shared';
+import { getUserNotifications } from '../../api';
+import { ErrorPage } from '../../pages';
+import { Spinner } from './spinner';
 
-export const Notification = (notifications: INotification[]) => {
+export const Notification = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const jwt = localStorage.getItem('jwt') as string;
+
+  const {
+    data: response,
+    isLoading,
+    error,
+    refetch,
+  } = useQuery<DataResult<INotification[]>>({
+    queryKey: ['notification'],
+    queryFn: () => getUserNotifications(jwt),
+    enabled: false, // Fetch only when triggered
+  });
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+    setIsDropdownOpen((prev) => {
+      if (!prev) {
+        // Fetch notifications only when opening the dropdown
+        refetch();
+      }
+      return !prev;
+    });
   };
 
   return (
@@ -22,17 +47,35 @@ export const Notification = (notifications: INotification[]) => {
 
       {isDropdownOpen && (
         <div className="absolute right-0 mt-2 w-64 rounded-lg bg-white shadow-lg border border-gray-200 z-10">
-          <div className="flex flex-col p-2">
-            {notifications.map((notification, index) => (
-              <Link
-                to="#"
-                key={index}
-                className="p-3 text-gray-600 hover:bg-gray-100 hover:text-orange-800 rounded-lg transition-all duration-200 cursor-pointer"
-              >
-                {notification.message}
-              </Link>
-            ))}
-          </div>
+          {isLoading ? (
+            <Spinner />
+          ) : error ? (
+            <ErrorPage
+              errorMessage="Error fetching the data"
+              errorDetails="Sorry, we can't fetch data. Please try again later."
+              statusCode={500}
+              icon={
+                <FontAwesomeIcon
+                  icon={faTriangleExclamation}
+                  className="text-orange-600 text-8xl mb-4 animate-bounce"
+                />
+              }
+            />
+          ) : (
+            <div className="flex flex-col p-2">
+              {response?.data.map(
+                (notification: INotification, index: number) => (
+                  <Link
+                    to="#"
+                    key={index}
+                    className="p-3 text-gray-600 hover:bg-gray-100 hover:text-orange-800 rounded-lg transition-all duration-200 cursor-pointer"
+                  >
+                    {notification.message}
+                  </Link>
+                ),
+              )}
+            </div>
+          )}
         </div>
       )}
     </div>
